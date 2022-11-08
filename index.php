26355db68ee376be70c021b2a5b0a159fa69251e
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2022-11-09 00:51:23 +0100

    Dodat kostur za stranicu za prijavu

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prijava</title>
+</head>
+<body>
+<div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form>
+        <h3>Prijava</h3>
+
+        <label for="username">Prijava</label>
+        <input type="text" placeholder="Email or Phone" id="username">
+
+        <label for="password">Sifra</label>
+        <input type="password" placeholder="Password" id="password">
+
+        <button>prijava</button>
+    </form>
+</body>
+</html>
