f35d7a94433082be81e9fce0efce93aa3383c339
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2022-11-12 00:15:58 +0100

    Kreiran model za izdavanje knjiga

---
+++ model/Renting.php
@@ -0,0 +1,33 @@
+<?php
+class Renting
+{
+    public $idRenting;
+    public $status;
+    public $date;
+    public $isbnBook;
+    public $idBook;
+    public $idMember;
+
+    public function __construct($idRenting = null, $status = null, $date = null, $isbnBook = null, $idBook = null, $idMember = null)
+    {
+        $this->idRenting = $idRenting;
+        $this->status = $status;
+        $this->date = $date;
+        $this->isbnBook = $isbnBook;
+        $this->idBook = $idBook;
+        $this->idMember = $idMember;
+    }
+
+    public static function addRenting($renting, mysqli $conn)
+    {
+        $query = "INSERT INTO `renting`(status,date,isbnBook,idBook,idMember) VALUES('$renting->status',
+        $renting->date,
+        $renting->isbnBook,
+        $renting->idBook,
+        $renting->idMember)";
+        $result = $conn->query($query);
+        return $result;
+    }
+
+}
+?>
